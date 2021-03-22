89604dd57013a20ee8493258f9ae4656ed4511d5
Igor Mandrichenko <igorvm@gmail.com>
2021-03-22 15:27:55 -0400

    .

--- webpie/__init__.py
+++ webpie/__init__.py
@@ -3,7 +3,6 @@ from .WPSessionApp import WPSessionApp
 #from .HTTPServer import (HTTPServer, HTTPSServer, run_server)
 from .uid import uid
 from .HTTPServer import run_server, HTTPServer
-from .multiserver import MultiServer
 from .logs import Logger, Logged
 
 __all__ = [ "WPApp", "WPHandler", "Response", 
